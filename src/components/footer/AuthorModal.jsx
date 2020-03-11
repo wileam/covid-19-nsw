558f725b1b79083bafb9f78f9f95326188894212
Ivan Fan <fanyfivan@gmail.com>
2020-03-11 22:54:45 +1100

    add use at your own risk

--- src/components/footer/AuthorModal.jsx
+++ src/components/footer/AuthorModal.jsx
@@ -50,6 +50,11 @@ export const AuthorModal = () => (
           <List.Item>
           Authors are not responsible for any errors or omissions, or for the results obtained from the use of this information. All information in this site is provided "as is", with no guarantee of completeness, accuracy, timeliness or of the results obtained from the use of this information
           </List.Item>
+          <List.Item>
+            We do not make any warranties about the completeness, reliability and accuracy of these information.
+            None of the authors, contributors, adminstrators or anyone else connected with this website, in anyway whatsoever, can be responsible for your use of the information contained in or linked from these web pages.
+            Any action you take upon the information on this website is strictly at your own risk. and we will not be liable for any losses and damages in connection with the use of our website.
+          </List.Item>
         </List>
       </Modal.Description>
     </Modal.Content>
