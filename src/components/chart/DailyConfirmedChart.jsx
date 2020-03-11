ee9cf3c5e7eaabafb8a5aed37bd42e9216317314
Joanna Wu <joanna.wu@iress.com>
2020-03-11 21:35:02 +1100

    hide prediction line by default
    ... and change the line style to dotted

--- src/components/chart/DailyConfirmedChart.jsx
+++ src/components/chart/DailyConfirmedChart.jsx
@@ -5,7 +5,10 @@ import { dailyData, predictedData } from '../../data';
 const dailyChartData = {
   todayData: dailyData.map(([date, today]) => [new Date(date), today]),
   totalData: dailyData.map(([date, _today, total]) => [new Date(date), total]),
-  predictData: predictedData.map(([date, predictedTotal]) => [new Date(date), Math.round(predictedTotal)]),
+  predictData: predictedData.map(([date, predictedTotal]) => [
+    new Date(date),
+    Math.round(predictedTotal)
+  ]),
   deathData: dailyData.map(([date, _today, _total, death]) => [
     new Date(date),
     death
@@ -15,7 +18,15 @@ const dailyChartData = {
 const getOptions = data => {
   return {
     legend: {
-      show: true
+      show: true,
+      selected: {
+        // selected'series 1'
+        'total confirmed cases': true,
+        // unselected'series 2'
+        'predicted total confirmed cases': false,
+        'new cases on the day': true,
+        'new death cases': true
+      }
     },
     tooltip: {
       show: true
@@ -35,7 +46,16 @@ const getOptions = data => {
       {
         type: 'line',
         name: 'predicted total confirmed cases',
-        data: data.predictData
+        data: data.predictData,
+        smooth: false,
+        itemStyle: {
+          normal: {
+            lineStyle: {
+              width: 2,
+              type: 'dotted' //'dotted'虚线 'solid'实线
+            }
+          }
+        }
       },
       { type: 'bar', name: 'new cases on the day', data: data.todayData },
       { type: 'bar', name: 'new death cases', data: data.deathData }
