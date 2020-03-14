b4aabb5481a516c7d29178b77306de2e350ebd4a
ngot <zhuanghengfei@gmail.com>
2020-03-14 19:13:45 +1100

    fetch Airtable data on CI

---
+++ bin/fetchAirtable.js
@@ -0,0 +1,96 @@
+#!/usr/bin/env node
+
+const Airtable = require('airtable');
+const moment = require('moment');
+const fs = require('fs');
+const path = require('path');
+const _ = require('lodash');
+
+const base = Airtable.base(process.env.BASE_ID);
+
+const todayDateString = moment().format("YYYY-MM-DD");
+
+function getSummaryByDate(records, dateString) {
+  const date = moment(dateString);
+  records = records.filter(record => {
+    const d = moment(record.get('Diagnosis'));
+    if (date.isAfter(d)) {
+      return true;
+    }
+
+    if (!date.isAfter(d) && !date.isBefore(d)) {
+      return true;
+    }
+
+    return false;
+  });
+
+  const todayRecords = [];
+  const recoveredRecords = [];
+  const deathRecords = [];
+
+  records.forEach(record => {
+    if (dateString === record.get('Diagnosis')) {
+      todayRecords.push(record);
+    }
+    if ('discharged' === record.get('Status')) {
+      recoveredRecords.push(record);
+    }
+    if ('death' === record.get('Status')) {
+      deathRecords.push(record);
+    }
+  });
+
+  const totalRemianNumber = records.length - recoveredRecords.length - deathRecords.length;
+
+  return {
+    totalConfirmedNumber: records.length,
+    totalRecoveredNumber: recoveredRecords.length,
+    totalDeathNumber: deathRecords.length,
+    totalRemianNumber,
+    todayNewNumber: todayRecords.length,
+  };
+}
+
+function dailyHistory(records) {
+  const uniqRecords = _.uniqBy(records, e => e.get('Diagnosis'));
+  const dailySummaryHistory = [];
+  for (let i = uniqRecords.length - 1; i >= 0; i--) {
+    const record = uniqRecords[i];
+    const date = record.get('Diagnosis');
+    const summary = getSummaryByDate(records, date);
+    dailySummaryHistory.push(
+      {
+        date,
+        todayNewNumber: summary.todayNewNumber,
+        totalConfirmedNumber: summary.totalConfirmedNumber
+      }
+    );
+  }
+  return dailySummaryHistory;
+}
+
+(async () => {
+  let records = await base('NSW').select({
+    maxRecords: 2000,
+    view: "All confirmed cases"
+  }).all();
+
+  const dailySummaryPath = path.join(process.cwd(), 'src/data/todaySummary.js');
+  const todaySummarys = getSummaryByDate(records, todayDateString);
+  const dailySummaryTpl = `
+export const todaySummarys = ${JSON.stringify(todaySummarys, null, 2)};
+  `
+  console.log(dailySummaryTpl);
+  fs.writeFileSync(dailySummaryPath, dailySummaryTpl);
+
+  const dailyHistorys = dailyHistory(records);
+  console.log(dailyHistorys);
+  const dailyHistorysTpl = `
+export const dailyHistorys = ${JSON.stringify(dailyHistorys, null, 2)};
+  `
+  const dailyHistoryPath = path.join(process.cwd(), 'src/data/dailyHistory.js');
+  fs.writeFileSync(dailyHistoryPath, dailyHistorysTpl);
+})().catch(e => {
+  console.log(e);
+});
---
+++ src/data/dailyHistory.js
@@ -0,0 +1,84 @@
+
+export const dailyHistorys = [
+  {
+    "date": "2020-01-25",
+    "todayNewNumber": 3,
+    "totalConfirmedNumber": 3
+  },
+  {
+    "date": "2020-01-27",
+    "todayNewNumber": 1,
+    "totalConfirmedNumber": 4
+  },
+  {
+    "date": "2020-03-01",
+    "todayNewNumber": 2,
+    "totalConfirmedNumber": 6
+  },
+  {
+    "date": "2020-03-02",
+    "todayNewNumber": 3,
+    "totalConfirmedNumber": 9
+  },
+  {
+    "date": "2020-03-03",
+    "todayNewNumber": 6,
+    "totalConfirmedNumber": 15
+  },
+  {
+    "date": "2020-03-04",
+    "todayNewNumber": 7,
+    "totalConfirmedNumber": 22
+  },
+  {
+    "date": "2020-03-05",
+    "todayNewNumber": 3,
+    "totalConfirmedNumber": 25
+  },
+  {
+    "date": "2020-03-06",
+    "todayNewNumber": 3,
+    "totalConfirmedNumber": 28
+  },
+  {
+    "date": "2020-03-07",
+    "todayNewNumber": 8,
+    "totalConfirmedNumber": 36
+  },
+  {
+    "date": "2020-03-08",
+    "todayNewNumber": 4,
+    "totalConfirmedNumber": 40
+  },
+  {
+    "date": "2020-03-09",
+    "todayNewNumber": 7,
+    "totalConfirmedNumber": 47
+  },
+  {
+    "date": "2020-03-10",
+    "todayNewNumber": 14,
+    "totalConfirmedNumber": 61
+  },
+  {
+    "date": "2020-03-11",
+    "todayNewNumber": 4,
+    "totalConfirmedNumber": 65
+  },
+  {
+    "date": "2020-03-12",
+    "todayNewNumber": 13,
+    "totalConfirmedNumber": 78
+  },
+  {
+    "date": "2020-03-13",
+    "todayNewNumber": 14,
+    "totalConfirmedNumber": 92
+  },
+  {
+    "date": "2020-03-14",
+    "todayNewNumber": 20,
+    "totalConfirmedNumber": 112
+  }
+];
+  
