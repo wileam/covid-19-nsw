ae689bbfd59500452dc5ec3f7749f6ba52e208be
Kitman <kitmanwork@gmail.com>
2020-03-14 17:38:56 +1100

    Update data.js (#6)

--- src/data.js
+++ src/data.js
@@ -1,3 +1,21 @@
+
+/*
+export const VICData = [
+    ['March 1, 2020', 3, 10, 0],
+    ['March 2, 2020', 0, 10, 0],
+    ['March 3, 2020', 0, 10, 0],
+    ['March 4, 2020', 1, 13, 0],
+    ['March 5, 2020', 0, 14, 0],
+    ['March 6, 2020', 0, 14, 0],
+    ['March 7, 2020', 1, 14, 0],
+    ['March 8, 2020', 1, 15, 0],
+    ['March 9, 2020', 2, 16, 0],
+    ['March 10, 2020', 3, 18, 0],
+    ['March 11, 2020', 3, 21, 0],
+    ['March 12, 2020', 6, 27, 0],
+];
+*/
+
 // date, new today, total confirmed, total death
 export const dailyData = [
   // ['January 24, 2020', 0, 0, 0],
