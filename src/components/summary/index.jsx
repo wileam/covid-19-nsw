64d602a673426e9e1b0a545fa46584fe20601bcf
Joanna Wu <joanna_wu@live.com>
2020-03-15 00:15:22 +1100

    update summary styling

--- src/components/summary/index.jsx
+++ src/components/summary/index.jsx
@@ -1,41 +1,42 @@
 import React from 'react';
 import { Grid, Statistic, Table } from 'semantic-ui-react';
+import { todaySummarys } from '../../data/todaySummary';
 import { summaryData } from '../../data.js';
 import './index.scss';
 
 export const Summary = () => {
   const {
-    totalConfirmed,
-    remain,
-    recovered,
-    death,
-    wip,
-    today,
-    excluded,
-    totalTested
+    totalConfirmedNumber,
+    totalRecoveredNumber,
+    totalDeathNumber,
+    totalRemianNumber,
+    todayNewNumber,
+  } = todaySummarys;
+  const {
+    wip, totalTested, excluded
   } = summaryData;
   return (
     <div className='summary'>
       <h2 className='ui small header'>Summary data:</h2>
-      <Grid columns='equal' container>
+      <Grid columns='equal'>
         <Grid.Column>
-          <Statistic color='blue' label='Remain' value={remain} />
+          <Statistic color='blue' label='Remain' value={totalRemianNumber} />
         </Grid.Column>
         <Grid.Column>
-          <Statistic label='Total' value={totalConfirmed} />
+          <Statistic label='Total' value={totalConfirmedNumber} />
         </Grid.Column>
         <Grid.Column>
-          <Statistic color='grey' label='Death' value={death} />
+          <Statistic color='grey' label='Death' value={totalDeathNumber} />
         </Grid.Column>
         <Grid.Column>
-          <Statistic color='green' label='Recover' value={recovered} />
+          <Statistic color='green' label='Recover' value={totalRecoveredNumber} />
         </Grid.Column>
       </Grid>
       <Table unstackable compact>
         <Table.Body>
           <Table.Row negative>
             <Table.Cell>New cases today</Table.Cell>
-            <Table.Cell>{today}</Table.Cell>
+            <Table.Cell>{todayNewNumber}</Table.Cell>
           </Table.Row>
           <Table.Row>
             <Table.Cell>Cases under investigation</Table.Cell>
