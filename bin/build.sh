4dcfc7434c7d322c117220ae8a83328136607dcb
ngot <zhuanghengfei@gmail.com>
2020-03-14 15:47:45 +1100

    ignore generate update date locally

--- bin/build.sh
+++ bin/build.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-cp ./src/dateTime.tpl ./src/dateTime.current
-
-echo $(TZ=Australia/Sydney date +"'%B %d, %Y %T';") >> ./src/dateTime.current
-
-mv ./src/dateTime.current ./src/dateTime.js
+if [[ -n $CI ]]; then
+  echo "Building on CI, regenerate the updated date..."
+  cp ./src/dateTime.tpl ./src/dateTime.current
+  echo $(TZ=Australia/Sydney date +"'%B %d, %Y %T';") >> ./src/dateTime.current
+  mv ./src/dateTime.current ./src/dateTime.js
+else
+  echo "Not build on CI, ignore the regenerating update date..."
+fi
