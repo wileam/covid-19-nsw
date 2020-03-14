4dae46b215f061b7b18401cb60a286348922bcd3
Joanna Wu <joanna_wu@live.com>
2020-03-15 00:00:32 +1100

    update share button ui

--- src/components/share/index.jsx
+++ src/components/share/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Responsive, Segment } from 'semantic-ui-react';
+import { Responsive } from 'semantic-ui-react';
 import { Facebook, Twitter } from 'react-social-sharing';
 
 export const ShareButton = () => {
   const url = 'https://covid-19.wileam.com';
   return (
-    <Responsive as={Segment} {...Responsive.onlyLargeScreen}>
+    <Responsive {...Responsive.onlyComputer}>
       <p style={{textAlign: 'right'}}>
         Share the page:
         <Facebook link={url} />
