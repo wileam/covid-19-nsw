68c87e2258c764a222b2bf69ce9fadd35e811f1f
Joanna Wu <joanna_wu@live.com>
2020-03-15 02:33:10 +1100

    add react-router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Disqus from 'disqus-react';
 import { Summary, Chart, DetailTable, Footer, ShareButton } from './components';
-import { Divider } from 'semantic-ui-react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams
+} from 'react-router-dom';
+import { Divider, List } from 'semantic-ui-react';
+import { dailyData, predictedData, summaryData } from './data.js';
 import { updateTime } from './dateTime.js';
 
 import 'semantic-ui-css/semantic.min.css';
@@ -14,33 +22,73 @@ const disqusConfig = {
   title: 'CoVid-19 Updates - NSW'
 };
 
-function App() {
+const App = () => {
+  const [active, setActive] = useState('Australia');
+  const navList = ['Australia', 'NSW', 'VIC', 'QLD', 'WA', 'SA', 'TAS', 'NT', 'ACT']
+  return (
+    <Router>
+      <div className='ui container center aligned'>
+        <List link horizontal>
+          {navList.map(nav => (
+            <List.Item as='a' active={active === nav} onClick={() => setActive(nav)}>
+              <Link to={nav}>{nav}</Link>
+              </List.Item>
+          ))}
+        </List>
+      </div>
+      <Switch>
+        <Route path='/:id' children={<Child />} />
+      </Switch>
+      <Child />
+    </Router>
+  );
+};
+
+function getDataById(id) {
+  return {
+    chartData: {
+      dailyData,
+      predictedData
+    },
+    summaryData
+  };
+}
+
+function Child() {
+  // We can use the `useParams` hook here to access
+  // the dynamic pieces of the URL.
+  let { id } = useParams();
+  const data = getDataById(id);
   return (
     <div className='ui container'>
       <header>
-        <h1 className='ui header'>CoVid-19 Updates - NSW</h1>
+        <h1 className='ui header'>CoVid-19 Updates - {id || 'Australia'}</h1>
         <small className='ui small'>Site updated: {updateTime} AEDT</small>
       </header>
-      <ShareButton />
-    
-      <Summary />
-      <Divider />
-      <Chart />
+
+      <ShareButton id={id} />
+      <Page id={id} data={data} />
       <Divider />
 
       <DetailTable />
-
 
       <Disqus.DiscussionEmbed
         shortname={disqusShortname}
         config={disqusConfig}
       />
-
       <footer>
         <Footer />
       </footer>
     </div>
   );
 }
+
+const Page = ({ data }) => (
+  <>
+    <Summary data={data.summaryData} />
+    <Divider />
+    <Chart data={data.chartData} />
+  </>
+);
 
 export default App;
--- src/components/share/index.jsx
+++ src/components/share/index.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { Responsive } from 'semantic-ui-react';
 import { Facebook, Twitter } from 'react-social-sharing';
 
-export const ShareButton = () => {
-  const url = 'https://covid-19.wileam.com';
+export const ShareButton = ({id}) => {
+  const url = `https://covid-19.wileam.com/${id}`;
   return (
     <Responsive {...Responsive.onlyComputer}>
       <p style={{textAlign: 'right'}}>
