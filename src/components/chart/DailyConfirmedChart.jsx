4bfd07fc6b421feb36d338ca497cfe6d38836327
Henry Pan <henry.pan@iress.com>
2020-03-15 16:43:16 +1100

    fix white screen when change state

--- src/components/chart/DailyConfirmedChart.jsx
+++ src/components/chart/DailyConfirmedChart.jsx
@@ -1,81 +1,93 @@
-import React from 'react';
-import ReactEcharts from 'echarts-for-react';
+import React from "react";
+import ReactEcharts from "echarts-for-react";
 
 const getOptions = (id, dailyHistorys, predicts) => {
-  let todayData = dailyHistorys.map(({date, todayNewNumber}) => [new Date(date), todayNewNumber]);
-  let totalData = dailyHistorys.map(({date, totalConfirmedNumber}) => [new Date(date), totalConfirmedNumber]);
-  let predictData = predicts.map(({date, predictedTotalConfirmedNumber}) => [
+  let todayData = dailyHistorys.map(({ date, todayNewNumber }) => [
+    new Date(date),
+    todayNewNumber
+  ]);
+  let totalData = dailyHistorys.map(({ date, totalConfirmedNumber }) => [
+    new Date(date),
+    totalConfirmedNumber
+  ]);
+  let predictData = predicts.map(({ date, predictedTotalConfirmedNumber }) => [
     new Date(date),
     predictedTotalConfirmedNumber
   ]);
+  const series = [
+    {
+      type: "line",
+      name: "total",
+      data: totalData,
+      itemStyle: {
+        normal: {
+          label: {
+            show: true
+          }
+        }
+      }
+    },
+    {
+      type: "bar",
+      name: "new cases on the day",
+      data: todayData,
+      itemStyle: {
+        normal: {
+          label: {
+            show: true,
+            position: "top"
+          }
+        }
+      }
+    }
+  ];
+  if (id === "NSW") {
+    series.push({
+      type: "line",
+      name: "predicted total",
+      data: predictData,
+      smooth: false,
+      itemStyle: {
+        normal: {
+          label: {
+            show: true,
+            position: "right"
+          },
+          lineStyle: {
+            width: 2,
+            type: "dotted" //'dotted'虚线 'solid'实线
+          }
+        }
+      }
+    });
+  }
+
   return {
     legend: {
       show: true,
       selected: {
         total: true,
-        'predicted total': false,
-        'new cases on the day': true
+        "predicted total": false,
+        "new cases on the day": true
       }
     },
     tooltip: {
       show: true
     },
     xAxis: {
-      type: 'time'
+      type: "time"
     },
     yAxis: {
-      type: 'value'
+      type: "value"
     },
-    series: [
-      {
-        type: 'line',
-        name: 'total',
-        data: totalData,
-        itemStyle: {
-          normal: {
-            label: {
-              show: true
-            }
-          }
-        }
-      },
-      id === 'NSW' && {
-        type: 'line',
-        name: 'predicted total',
-        data: predictData,
-        smooth: false,
-        itemStyle: {
-          normal: {
-            label: {
-              show: true,
-              position: 'right'
-            },
-            lineStyle: {
-              width: 2,
-              type: 'dotted' //'dotted'虚线 'solid'实线
-            }
-          }
-        }
-      },
-      {
-        type: 'bar',
-        name: 'new cases on the day',
-        data: todayData,
-        itemStyle: {
-          normal: {
-            label: {
-              show: true,
-              position: 'top'
-            }
-          }
-        }
-      }
-    ]
+    series
   };
 };
 
-export const DailyConfirmedChart = ({id, dailyHistorys, predicts}) => (
-  <>
-    <ReactEcharts option={getOptions(id, dailyHistorys, predicts)} />
-  </>
-);
+export const DailyConfirmedChart = ({ id, dailyHistorys, predicts }) => {
+  return (
+    <>
+      <ReactEcharts option={getOptions(id, dailyHistorys, predicts)} />
+    </>
+  );
+};
