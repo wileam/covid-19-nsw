ec4bf4d02ac3bfa43a2709b53851e3f3d724fb8f
Joanna Wu <joanna_wu@live.com>
2020-03-17 23:55:15 +1100

    update buy me coffee

---
+++ src/components/footer/BuyCoffeeModal.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Modal, Grid } from 'semantic-ui-react';
+
+export const BuyCoffeeModal = () => (
+  <Modal
+    trigger={
+      // eslint-disable-next-line
+      <a className='bmc-button' href='#' onClick={e => e.preventDefault()}>
+        <img
+          src='https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg'
+          alt='Buy me a coffee'
+        />
+        <span style={{ marginLeft: '7px', fontSize: '14px' }}>
+          Buy me a coffee
+        </span>
+      </a>
+    }
+  >
+    <Modal.Header>Buy me a coffee</Modal.Header>
+    <Modal.Content>
+      <Modal.Description>
+        <Grid textAlign='center' columns='equal'>
+          <Grid.Row>
+            <Grid.Column>
+              <div>
+                <img src='./beem.svg' alt='beemit logo' width='80px' />
+                <img
+                  src='./beemit.jpg'
+                  alt='beemit'
+                  width='80px'
+                  height='80px'
+                />
+              </div>
+            </Grid.Column>
+            <Grid.Column>
+              <img src='./alipay.jpeg' alt='alipay' width='100px' />
+            </Grid.Column>
+            <Grid.Column>
+              <img src='./wechat.jpeg' alt='wechat' width='100px' />
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <a
+              target='_blank'
+              rel='noopener noreferrer'
+              href='https://www.buymeacoffee.com/6bpOFt7'
+            >
+              Paypal or card
+            </a>
+          </Grid.Row>
+        </Grid>
+      </Modal.Description>
+    </Modal.Content>
+  </Modal>
+);
--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,26 +1,14 @@
 import React from 'react';
 import { AuthorModal } from './AuthorModal';
 import { DisclaimerModal } from './DisclaimerModal';
+import { BuyCoffeeModal } from './BuyCoffeeModal';
 import './index.scss';
 
 export const Footer = () => (
   <footer>
     <p>
         If you found this site helpful, consider{' '}
-        <a
-          className='bmc-button'
-          target='_blank'
-          rel='noopener noreferrer'
-          href='https://www.buymeacoffee.com/6bpOFt7'
-        >
-          <img
-            src='https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg'
-            alt='Buy me a coffee'
-          />
-          <span style={{ marginLeft: '7px', fontSize: '14px' }}>
-            Buy me a coffee
-          </span>
-        </a>
+        <BuyCoffeeModal />
     </p>
     <p>
       <small>
