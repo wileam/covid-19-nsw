8291022902933a21ffbb86a3b6561da591e18a3c
Joanna Wu <joanna_wu@live.com>
2020-03-19 00:54:31 +1100

    fix styling

--- src/components/aus/index.jsx
+++ src/components/aus/index.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { DailyConfirmedChart } from '../chart/DailyConfirmedChart';
 import { AusSummary } from './AusSummary';
 import './index.scss';
+import { Divider } from 'semantic-ui-react';
 
 export const AusPage = ({ id, data }) => {
   return (
     <>
       <AusSummary id={id} data={data} />
+      <Divider />
+      <h2 className='ui small header'>Trending:</h2>
       <DailyConfirmedChart
         id={id}
         dailyHistorys={data[id].dailyHistorys}
